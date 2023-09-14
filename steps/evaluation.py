a49ea45f62f655782531ad7a011702fe94b1bb51
bala1802 <bala1802@live.com>
2023-09-14 19:11:16 +0530

    Added the Steps to evaluate the model

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -2,12 +2,40 @@ import logging
 import pandas as pd
 
 from zenml import step
+from sklearn.base import RegressorMixin
+from typing import Tuple
+from typing_extensions import Annotated
+from src.evaluation import MSE, R2, RMSE
 
-def evaluate_model(df: pd.DataFrame) -> None:
+@step
+def evaluate_model(model: RegressorMixin,
+                   X_test: pd.DataFrame,
+                   y_test: pd.DataFrame) -> [
+                       Annotated[float, "r2"],
+                       Annotated[float, "rmse"]
+                   ]:
     """
     Evaluates the model on the ingested data
 
     Args:
-        df: the ingested data
+        model: RegressorMixin
+        X_test: the test data
+        y_test: the test label
     """
-    pass
+
+    try:
+        prediction = model.predict(X_test)
+        mse_class = MSE()
+        mse = mse_class.calculate_scores(y_test, prediction)
+
+        r2_class = R2()
+        r2 = r2_class.calculate_scores(y_test, prediction)
+        
+        rmse_class = RMSE()
+        rmse = rmse_class.calculate_scores(y_test, prediction)
+
+        return r2, rmse
+    
+    except Exception as e:
+        logging.error("Error while calculating the Evaluation Scores: {}".format(e))
+        raise e
