e9f47bc72fe027864562db4d40a298cabc7030a9
bala1802 <bala1802@live.com>
2023-09-19 15:24:18 +0530

    added mlflow experiment trackers

--- steps/model_train.py
+++ steps/model_train.py
@@ -1,14 +1,19 @@
 import logging
 import pandas as pd
 
+import mlflow
+
 from zenml import step
+from zenml.client import Client
 
 from src.model_dev import LinearRegressionModel
 from sklearn.base import RegressorMixin
 
 from .config import ModelNameConfig
 
-@step
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def train_model(X_train: pd.DataFrame,
                 X_test: pd.DataFrame,
                 y_train: pd.DataFrame,
@@ -29,6 +34,7 @@ def train_model(X_train: pd.DataFrame,
     try:
         model = None
         if config.model_name == "LinearRegression":
+            mlflow.sklearn.autolog()
             model = LinearRegressionModel()
             trained_model = model.train(X_train=X_train, y_train=y_train)
             return trained_model
